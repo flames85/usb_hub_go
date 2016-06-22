dd2f9e0c9d952738ccb1b2f5a06eb091c074eb2c
flames85 <flames85@163.com>
2016-06-22 18:50:29 +0800

    first commit

---
+++ common.h
@@ -0,0 +1,11 @@
+#define UEVENT_BUFFER_SIZE      2048
+#define DEV_HUB_PREFIX_SIZE		1024
+#define	DEV_HUB_PREFIX_CONF	    "usb_hub_prefix.conf"
+
+#define DEFAULT_SPEED           9600
+#define DEFAULT_DATABITS        8
+#define DEFAULT_PARITY          'N'
+#define DEFAULT_STOPBITs        1
+
+
+
